a8f7dad16ec7644078fa7e2beb2fa1d56135f4bd
Anthony Bashi <bashia@uvic.ca>
2015-12-29 00:43:40 -0800

    Started to add setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name = "markovlib",
+	version = "0.1",
+	description = "Simple implementation of Markov Modeller for abstract lists.",
+	author = "Tony Bashi",
+	author_email = "bashia@uvic.ca",
+	license = "GPL v2",
+	packages = ["markovlib"],
+	zip_safe=False
+	)
